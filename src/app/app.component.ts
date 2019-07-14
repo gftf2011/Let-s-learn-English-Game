bd0705eaeeea75f5a44dab6fb975ef14e2f48b0d
Gabriel Ferrari Tarallo Ferraz <gftf2011@gmail.com>
2019-07-14 17:27:29 -0300

    Mudança de arquivos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,28 @@ import { EndGame } from './models/endGame.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  private correctAnswers:number
+  private totalQuestions:number
+
   private message:string
   private emoji:string
 
   private gameHasEnded:boolean
 
   public endgameEvent(endGame: EndGame): void {
+    this.correctAnswers = endGame.getCorrectAnswer()
+    this.totalQuestions = endGame.getTotalQuestions()
     this.message = endGame.getMessage()
     this.emoji = endGame.getEmoji()
     this.gameHasEnded = endGame.getGameHasEnded()
+  }
+
+  public get getCorrectAnswers():number {
+    return this.correctAnswers
+  }
+
+  public get getTotalQuestions():number {
+    return this.totalQuestions
   }
 
   public get getGameHasEnded():boolean {
@@ -29,6 +42,10 @@ export class AppComponent {
 
   public get getEmoji():string {
     return this.emoji
+  }
+
+  public resetGame():void {
+    this.gameHasEnded = false
   }
 
 }
